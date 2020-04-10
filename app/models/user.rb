61eba3a3560d9bbdf1a493e21394b1345a78aaff
Cody <lawdermiltcody@gmail.com>
2020-04-10 02:12:00 -0500

    admin complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :articles
+    has_many :articles, dependent: :destroy
     has_secure_password
     validates :username, presence: true, uniqueness: { case_sensitive: false },length: {minimum: 3, maximum: 25}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
